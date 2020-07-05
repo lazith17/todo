5c5c49586332301e8cacb44ce66b6a3ec2417af6
lazith15 <lazith15@gmail.com>
2020-07-05 20:32:25 +0530

    report generator

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_jjhm#&qjqbm_qula%xs2(0&^qwai@2c%au&4z57%g+=-lsbno'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lasith17.pythonanywhere.com']
 
 
 # Application definition
@@ -123,3 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_URL = '/login'
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
